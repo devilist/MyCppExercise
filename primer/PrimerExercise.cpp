8cef5c9da49556ed3426358d8fcce5e444683e1c
devilist <bit_zengpu@163.com>
2019-04-26 20:22:38 +0800

    vector

--- primer/PrimerExercise.cpp
+++ primer/PrimerExercise.cpp
@@ -2,8 +2,23 @@
 // Created by admin on 2019/4/10.
 //
 
+#include "h/prime_chapter3.h"
 #include "PrimerExercise.h"
 
-void runPrimeExercise() {
+void runExercise(int chapter);
 
+void runPrimeExercise() {
+    int chapter = 3;
+    runExercise(chapter);
 };
+
+void runExercise(int chapter) {
+    switch (chapter) {
+        case 3:
+            exercise3();
+            break;
+        default:
+            break;
+    }
+
+}
