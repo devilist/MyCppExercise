ee921640cff627e1ceda1523ea31375d71a7e516
ZengPu <zengpu@ufotosoft.com>
2019-05-10 17:05:56 +0800

    9 11

--- primer/prime_chapter11.cpp
+++ primer/prime_chapter11.cpp
@@ -2,8 +2,29 @@
 // Created by ZengPu on 2019-05-08.
 //
 
+
 #include <iostream>
+
+// 关联容器
 #include <map>
+#include <hash_map>
+#include <unordered_map>
+#include <set>
+#include <hash_set>
+#include <unordered_set>
+
+// 顺序容器
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <array>
+#include <string>
+
+// 容器适配器
+#include <stack>
+#include <queue>
+
 #include "h/prime_chapter11.h"
 
 using namespace std;
@@ -16,4 +37,31 @@ void exercise_11() {
 
 void mapRun() {
     map<string, int> m_Map;
+    multimap<string, int> m_MultiMap;
+    unordered_map<string, int> m_UnMap;
+    unordered_multimap<string, int> m_UnMultiMap;
+
+    set<string> m_Set;
+    multiset<string> m_MultiSet;
+    unordered_set<string> m_UnSet;
+    unordered_multiset<string> m_UnMultiSet;
+
+    // 栈适配器
+    stack<int> intStack;
+    for (int i = 0; i < 10; ++i) {
+        intStack.push(i);
+    }
+    while (!intStack.empty()) {
+        int val = intStack.top();
+        cout << val;
+        intStack.pop();
+    }
+    cout << endl;
+
+    // 队列适配器  fifo
+    queue<int> intQueue;
+
+
 }
+
+
