05ffeef88ed7a1a9005d46ada7cf72b48e7df0e3
ZengPu <zengpu@ufotosoft.com>
2019-05-10 15:26:27 +0800

    15

--- primer/prime_chapter15.cpp
+++ primer/prime_chapter15.cpp
@@ -6,10 +6,22 @@
 #include "class/Quote.h"
 #include "class/BulkQuote.h"
 
+void copyPtr(BulkQuote *bulkQuote);
+
 void exercise_15() {
 
     BulkQuote quote("book1", 12.34);
 
     quote.net_price(3);
+    copyPtr(&quote);
+    std::cout << "quote: " << &quote << std::endl;
 
+}
+
+void copyPtr(BulkQuote *bulkQuote) {
+    BulkQuote *b{nullptr};
+    b = bulkQuote;
+    b = nullptr;
+    std::cout << "b: " << b << std::endl;
+    std::cout << "bulkQuote: " << bulkQuote << std::endl;
 }
